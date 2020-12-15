da69c752358088811d33d2a87d0036c42860028b
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-12-15 13:55:00 +0530

    Add React Components and GET data from django server

---
+++ frontend/gui/src/containers/ArticleListView.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Axios from "axios";
+
+import Articles from "../components/Articles";
+
+class ArticleList extends Component {
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    Axios.get("http://127.0.0.1:8000/api/").then((res) => {
+      this.setState({
+        articles: res.data,
+      });
+      console.log(res.data);
+    });
+  }
+
+  render() {
+    return <Articles data={this.state.articles} />;
+  }
+}
+
+export default ArticleList;
